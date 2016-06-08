70d38a8c8dd64525dfeb5fcb44974c575279ecbe
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-06-08 16:48:47 -0500

    Changed script to decompile android framework classes, and updated android.jar to level 23

--- decompile.sh
+++ decompile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ANDROID_JARS_PATH="soot/platforms"
-JAVA_CLASSPATH="soot/axml-2.0.jar:soot/slf4j-api-1.7.5.jar:soot/slf4j-simple-1.7.5.jar:soot/soot-infoflow-android.jar:soot/soot-infoflow.jar:soot/soot-trunk.jar:soot/baksmali-2.1.1.jar:."
+JAVA_CLASSPATH="soot/axml-2.0.jar:soot/slf4j-api-1.7.5.jar:soot/slf4j-simple-1.7.5.jar:soot/soot-infoflow-android.jar:soot/soot-infoflow.jar:soot/soot-trunk.jar:soot/baksmali-2.1.1.jar:soot/platforms/android--1/android.jar:."
 
 APK_FILE=$1
 SOOT_OUT_DIR=$2
@@ -12,9 +12,9 @@ if [ -z "$APK_FILE" ] | [ -z "$SOOT_OUT_DIR" ]; then
 fi
 
 PROCESS_THIS=" -process-dir $APK_FILE"
-SOOT_CLASSPATH="${APK_FILE}"
+SOOT_CLASSPATH="${APK_FILE}:/usr/lib/jvm/default-java/jre/lib/rt.jar:/usr/lib/jvm/default-java/jre/lib/jce.jar"
 
-SOOT_CMD="soot.Main -d $SOOT_OUT_DIR -force-android-jar $ANDROID_JARS_PATH -allow-phantom-refs -src-prec apk -ire -f J $PROCESS_THIS"
+SOOT_CMD="soot.Main -pp -cp $SOOT_CLASSPATH -d $SOOT_OUT_DIR -android-jars $ANDROID_JARS_PATH -src-prec apk -allow-phantom-refs -app -i java -ire -f J $PROCESS_THIS"
 
 java -Xss50m -Xmx1500m -classpath ${JAVA_CLASSPATH} ${SOOT_CMD}
 
